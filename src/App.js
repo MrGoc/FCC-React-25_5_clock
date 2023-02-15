a632bacb480c2b47730e73b248a9d9e8079c689a
MrGoc <mrgoc@live.com>
2023-02-15 10:29:01 +0100

    CSS modification and finishing basic design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowUp,
+  faArrowDown,
+  faPlay,
+  faPause,
+  faRepeat,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="app">
       <h1>25 + 5 Clock</h1>
       <SessionSettings />
+      <Timer />
     </div>
   );
 }
@@ -15,23 +22,42 @@ function SessionSettings() {
   return (
     <div className="sessionSettings">
       <div>
-        <h2 id="break-label">Break Length</h2>
-        <button>
-          <FontAwesomeIcon id="break-decrement" icon={faArrowDown} />
+        <h3 id="break-label">Break Length</h3>
+        <button id="break-decrement">
+          <FontAwesomeIcon icon={faArrowDown} size="lg" />
         </button>
         <span id="break-length">5</span>
-        <button>
-          <FontAwesomeIcon id="break-increment" icon={faArrowUp} />
+        <button id="break-increment">
+          <FontAwesomeIcon icon={faArrowUp} size="lg" />
         </button>
       </div>
       <div>
-        <h2 id="session-label">Session Length</h2>
-        <button>
-          <FontAwesomeIcon id="session-decrement" icon={faArrowDown} />
+        <h3 id="session-label">Session Length</h3>
+        <button id="session-decrement">
+          <FontAwesomeIcon icon={faArrowDown} size="lg" />
         </button>
         <span id="session-length">25</span>
-        <button>
-          <FontAwesomeIcon id="session-increment" icon={faArrowUp} />
+        <button id="session-increment">
+          <FontAwesomeIcon icon={faArrowUp} size="lg" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Timer() {
+  return (
+    <div className="timer">
+      <div className="timerDisplay">
+        <h3 id="timer-label">Session</h3>
+        <p id="time-left">25:00</p>
+      </div>
+      <div className="timerButtons">
+        <button id="start_stop">
+          <FontAwesomeIcon icon={faPlay} size="lg" />
+        </button>
+        <button id="reset">
+          <FontAwesomeIcon icon={faRepeat} size="lg" />
         </button>
       </div>
     </div>
