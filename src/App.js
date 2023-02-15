badca095e39ab72a3fcd06cb4cbc6fab264d3edc
MrGoc <mrgoc@live.com>
2023-02-15 08:07:38 +0100

    Inital.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>25 + 5 Clock</h1>
+      <SessionSettings />
+    </div>
+  );
+}
+
+function SessionSettings() {
+  return (
+    <div className="sessionSettings">
+      <div>
+        <h2 id="break-label">Break Length</h2>
+        <button>
+          <FontAwesomeIcon id="break-decrement" icon={faArrowDown} />
+        </button>
+        <span id="break-length">5</span>
+        <button>
+          <FontAwesomeIcon id="break-increment" icon={faArrowUp} />
+        </button>
+      </div>
+      <div>
+        <h2 id="session-label">Session Length</h2>
+        <button>
+          <FontAwesomeIcon id="session-decrement" icon={faArrowDown} />
+        </button>
+        <span id="session-length">25</span>
+        <button>
+          <FontAwesomeIcon id="session-increment" icon={faArrowUp} />
+        </button>
+      </div>
     </div>
   );
 }
